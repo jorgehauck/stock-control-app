b7eb4425b925cfd7d2183cd85ab988cb47f19bf2
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-09-01 18:42:51 -0300

    Implementando formulário reativo

--- stock-control-app/src/app/modules/home/home.component.ts
+++ stock-control-app/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  singnUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  public onSubmit(): void {
+    console.log("DADOS DO FORMULÁRIO DE LOGIN", this.loginForm.value);
+  }
+
+  public onSubmitSingnUpForm(): void {
+    console.log("DADOS DO FORMULÁRIO DE CRIAÇÃO DE CONTA", this.singnUpForm.value);
+  }
+
+
 }
